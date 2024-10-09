2dc1073b42cfc08a5e46d2a27df63f4706afaf06
d1mitrina <159777410+d1mitrina@users.noreply.github.com>
2024-10-09 18:24:57 +0100

    Add files via upload

--- main2.py
+++ main2.py
@@ -1,17 +1,44 @@
-#Applying median blurring tequnique to an image, Applying bilateral filter - smooths out image 
-import os, cv2 
-import numpy as np 
-image=cv2.imread("cat.jpg")
-#median blur : used in digital proccesses / keeps the edges and removes the noise in the image 
-median=cv2.medianBlur(image,5,0)
-cv2.imshow("Median",median)
+import cv2
+import numpy as np
+blobby = cv2.imread("circles.jpeg")
 
-#bilateral filter - opposite of median 
-#smooths images perserving the edges
-bilateral = cv2.bilateralFilter(image, 5,70,70) # image, diameter of pixels, sigma color, sigma space
-cv2.imshow("Bilateral",bilateral)
+#getting paramters 
+params = cv2.SimpleBlobDetector_Params()
+
+#parameter for area filtering 
+params.filterByArea = True 
+params.minArea = 100 
+
+#parameter for setting circular filtering 
+params.filterByCircularity = True 
+params.minCircularity = 0.9 
+
+#parameter for setting convexity
+#Convexity, in the context of shapes or objects
+#refers to a property where any line segement drawn between two points on the boundry of the shape lies entirely within the shape
+#Example of convex shapes include circles,ellipses,triangles,rectangles, and polygons
+#These shapes have a consistent curvature and do not contain any inward 'curves' or indentations 
+params.filterByConvexity = True 
+params.minConvexity = 0.2
+
+#parameter for inertia 
+params.filterByInertia = True 
+params.minInertiaRatio = 0.01
+
+detector = cv2.SimpleBlobDetector_create(params)
+circles = detector.detect(blobby)
+
+#drawing circles on the image 
+#np.#zeros is a NumPy function that creates a new list/array with zeros. It is often used to initialise arrays before filling them with actual data 
 
 
+blank = np.zeros((1,1)) #one row one column 
 
+blobs = cv2.drawKeypoints(blobby,circles,blank,(200,10,30),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS) #whereever you found the keypoints of the circle - draw them there
+noblobs = len(circles)
+text = "Number of circular Blobs: "+str(noblobs)
+cv2.putText(blobs,text,(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,0),2)
+
+cv2.imshow("Blob",blobs)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
